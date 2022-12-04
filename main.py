7c708d769e3b5aeecd09866c8d77e0dc441ae10f
Eduardo <fxr.ed03@gmail.com>
2022-12-04 11:30:15 -0300

    PEP8 code formatting kickstart

--- main.py
+++ main.py
@@ -6,78 +6,89 @@ import os
 import audioread
 
 
-filenamesMP3 = [os.path.basename(filename) for filename in glob.glob("IO/*.mp3")]
-filenamesWAV = [os.path.basename(filename) for filename in glob.glob("IO/*.wav")]
-filenamesFLAC = [os.path.basename(filename) for filename in glob.glob("IO/*.flac")]
+filenames_mp3 = [os.path.basename(filename) for
+                filename in glob.glob("IO/*.mp3")]
 
-audioFilenames = filenamesFLAC + filenamesMP3 + filenamesWAV
+filenames_wav = [os.path.basename(filename) for
+                filename in glob.glob("IO/*.wav")]
+
+filenames_flac = [os.path.basename(filename) for
+                filename in glob.glob("IO/*.flac")]
+
+audio_filenames = filenames_flac + filenames_mp3 + filenames_wav
 
 
-if len(audioFilenames) > 1:
+if len(audio_filenames) > 1:
 
     print("Which audio file do you want to use?")
 
-    for filename in audioFilenames:
-        print("{}: {}".format(audioFilenames.index(filename)+1, filename))
+    for filename in audio_filenames:
+        print("{}: {}".format(audio_filenames.index(filename)+1, filename))
 
-    desiredAudioFilenameIndex = int(input()) - 1
-    desiredAudioFilenameString = audioFilenames[desiredAudioFilenameIndex]
+    desired_audio_filename_index = int(input()) - 1
+    desired_audio_filename_string = audio_filenames[desired_audio_filename_index]
 
-elif len(audioFilenames) == 1:
-    desiredAudioFilenameString = audioFilenames[0]
+elif len(audio_filenames) == 1:
+    desired_audio_filename_string = audio_filenames[0]
 
 else:
     print("No audio files! Please insert an audio file into the IO directory.")
     sys.exit()
 
 
-with audioread.audio_open("IO/{}".format(desiredAudioFilenameString)) as f:
+with audioread.audio_open("IO/{}".format(desired_audio_filename_string)) as f:
     length = f.duration
 
 
-filenamesJPG = [os.path.basename(filename) for filename in glob.glob("IO/*.jpg")]
-filenamesPNG = [os.path.basename(filename) for filename in glob.glob("IO/*.png")]
+filenames_jpg = [os.path.basename(filename) for
+                filename in glob.glob("IO/*.jpg")]
 
-imageFilenames = filenamesJPG + filenamesPNG
+filenames_png = [os.path.basename(filename) for
+                filename in glob.glob("IO/*.png")]
+
+image_filenames = filenames_jpg + filenames_png
 
 
-if len(imageFilenames) > 1:
+if len(image_filenames) > 1:
 
     print("Which image file do you want to use?")
 
-    for filename in imageFilenames:
-        print("{}: {}".format(imageFilenames.index(filename)+1, filename))
+    for filename in image_filenames:
+        print("{}: {}".format(image_filenames.index(filename)+1, filename))
 
-    desiredImageFilenameIndex = int(input()) - 1
-    desiredImageFilenameString = imageFilenames[desiredImageFilenameIndex]
+    desired_image_filename_index = int(input()) - 1
+    desired_image_filename_string = image_filenames[desired_image_filename_index]
 
-elif len(imageFilenames) == 1:
-    desiredImageFilenameString = imageFilenames[0]
+elif len(image_filenames) == 1:
+    desired_image_filename_string = image_filenames[0]
 
 else:
     print("No image files! Please insert an image file into the IO directory.")
     sys.exit()
 
 
-imageFile = Image.open("IO/{}".format(desiredImageFilenameString))
-imageWidth = imageFile.width
-imageFile.close()
+image_file = Image.open("IO/{}".format(desired_image_filename_string))
+image_width = image_file.width
+image_file.close()
 
-if imageWidth >= 1440:
-    videoWidth = 1440
-elif imageWidth >= 1080 and imageWidth < 1440:
-    videoWidth = 1080
+if image_width >= 1440:
+    video_width = 1440
+elif image_width >= 1080 and image_width < 1440:
+    video_width = 1080
 else:
-    videoWidth = 720
+    video_width = 720
 
 
-videoInput = ffmpeg.input("IO/{}".format(desiredImageFilenameString), loop = 1, framerate = 1, t = length)
-audioInput = ffmpeg.input("IO/{}".format(desiredAudioFilenameString))
+video_input = ffmpeg.input("IO/{}".format(desired_image_filename_string),
+                           loop = 1, framerate = 1, t = length)
+audio_input = ffmpeg.input("IO/{}".format(desired_audio_filename_string))
 
 (
     ffmpeg
-    .concat(videoInput, audioInput, v = 1, a = 1)
-    .filter('scale', videoWidth, -1)
-    .output('IO/{}.mp4'.format(os.path.splitext(desiredAudioFilenameString)[0]), acodec = 'mp3', audio_bitrate = '320k')
+    .concat(video_input, audio_input, v = 1, a = 1)
+    .filter('scale', video_width, -1)
+    .output('IO/{}.mp4'.format(
+            os.path.splitext(desired_audio_filename_string)[0]),
+            acodec = 'mp3', audio_bitrate = '320k')
     .run(overwrite_output = True)
 )
